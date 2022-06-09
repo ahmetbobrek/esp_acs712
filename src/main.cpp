21021faf33015805c3e078999ecebefff86f06b4
DESKTOP-STBF4HO\ahmet <ahmetbobrek@gmail.com>
2022-06-09 16:03:12 +0300

    0906

--- src/main.cpp
+++ src/main.cpp
@@ -2,67 +2,33 @@
 #include "acs712.h"
 ACS712 acs712(ACS712_05B, 35);
 int16_t cal;
-
+ int16_t acc;
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Calibrating... Ensure that no current flows through the sensor at this moment");
-    int zero = acs712.calibrate(); 
- 
+    acs712.calibrate();
 }
 void loop()
 {
-    //float Irms=acs712.getCurrentAC();
-    //float I = acs712.getCurrentDC(); 
-    // Send it to serial
-    //Serial.println(String("I = ") + I + " A");
-    //Serial.println(String("Irms = ") + acs712.Irms+ " A");
-    Serial.println(analogRead(35));
-    /*float ACS712::getCurrentAC(uint16_t frequency){
-  period=1000000/frequency;
-  t_start=micros(); 
+    // We use 230V because it is the common standard in European countries
+  // Change to your local, if necessary
+  float U = 230;
 
+  // To measure current we need to know the frequency of current
+  // By default 50Hz is used, but you can specify desired frequency
+  // as first argument to getCurrentAC() method, if necessary
+  float I = acs712.getCurrentAC();
+
+  // To calculate the power we need voltage multiplied by current
+ 
+  float P = U * I;
+ /*Serial.println(String("I = ") + I + " A");
+  Serial.println(String("P = ") + P + " Watts");*/
+  Serial.println(I);
   
-  while (micros()-t_start<period)
-  {
-      lnow=analogRead(35)-zero;
-      lsum+=lnow*lnow;
-      measurements_count++;    
-      
-  }
-  float Irms=sqrt(lsum/measurements_count)/ADC_SCALE*VREF/sensitivity;
-  return lsum;
-}*/
-acs712.t_start=micros();
-for (int i = 0; i < 50; i++)
-{
-   acs712.lnow=analogRead(35)-2560;
-    acs712.lsum+=sq(acs712.lnow);
-    acs712.measurements_count++;
-    }
-    Serial.println(acs712.measurements_count);
-    Serial.println(acs712.lnow);
-    Serial.println(sq(acs712.lnow));
-    /*Serial.println(acs712.lsum);
-    Serial.println(sqrt(acs712.lsum)/acs712.measurements_count);*/
-    Serial.println(acs712.lsum);
-    Serial.println(sqrt(acs712.lsum/acs712.measurements_count)*3.3/4096/0.185);
-    Serial.println("zaman:"+String(micros()-acs712.t_start));
-     
-    Serial.println("************************");
-    acs712.lsum=0;
-    acs712.measurements_count=0;
-    delay(300);
+  
+ // Serial.println(acs712.shape());
 }
 
-
- /*while (micros()-acs712.t_start<20000)
-{
-    
-     
-
-    // Wait a second before the new measurement
-    delay(300);
-}*/
 
