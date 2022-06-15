631408b57ccf36a257b848312f225714664cb966
DESKTOP-STBF4HO\ahmet <ahmetbobrek@gmail.com>
2022-06-15 09:48:50 +0300

    150601

--- src/main.cpp
+++ src/main.cpp
@@ -104,19 +104,62 @@ void removeDir(fs::FS &fs,const char* path){
   }
   }
 
+void writeFile(fs::FS &fs,const char* path,const char* message){
+  Serial.printf("Writing file: %s\n",path);
+  File file=fs.open(path,FILE_WRITE);
+  if (!file)
+  {
+    Serial.println("Failed to open file for wwriting");
+    return;
+  }
+  if (file.print(message)){
+    Serial.println("File written");
+  }
+  else{
+    Serial.println("Write failed");
+  }
+  file.close();
+  }
+  /*
+  void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}*/
+void appendFile(fs::FS &fs, const char* path, const char* message){
+  Serial.printf("Appending to file: %s\n",path);
+
+  File file=fs.open(path,FILE_APPEND);
+  if (!file)
+  {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message))
+  {
+    Serial.println("Message appended");
+  }
+  else{
+    Serial.println("Appended failed");
+    return;
+  }
+  file.close();
+  
+
+}
 
 void setup()
 {
-    /* if(!SD.begin()){
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD.cardType();
-
-    if(cardType == CARD_NONE){
-        Serial.println("No SD card attached");
-        return;
-    }*/
     Serial.begin(115200);
     if (!SD.begin())
     {
@@ -124,10 +167,43 @@ void setup()
         return;
     }
     uint8_t cardType=SD.cardType();
-    if (cardType==)
+    if (cardType==CARD_NONE)
     {
-        /* code */
+        Serial.println("No SD card attached");
+        return;
     }
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+    createDir(SD,"/data");
+    /*listDir(SD, "/", 0);
+    createDir(SD, "/mydir");
+    listDir(SD, "/", 0);
+    removeDir(SD, "/mydir");
+    listDir(SD, "/", 2);*/
+
+    
+    writeFile(SD, "/hello.txt", "Hello ");
+    appendFile(SD, "/hello.txt", "World!\n");
+    /*readFile(SD, "/hello.txt");
+    deleteFile(SD, "/foo.txt");
+    renameFile(SD, "/hello.txt", "/foo.txt");
+    readFile(SD, "/foo.txt");
+    testFileIO(SD, "/test.txt");*/
+    
+   
+    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
     
     
     acs712.calibrate();
